205a0863f6ec97320ebd44f73b91490d3864869c
ilhom <ilhommamatqulov276@gmail.com>
2023-03-02 15:03:54 +0500

    randomuser

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from tinydb import TinyDB
+import requests
+from tinydb import TinyDB, Query
 
-db = TinyDB('data.json')
+response = requests.get("https://randomuser.me/api/")
+data = response.json()["results"][0]
+
+
+first_name = data["name"]["first"]
+last_name = data["name"]["last"]
+age = data["dob"]["age"]
+phone = data["phone"]
+country = data["location"]["country"]
+email = data["email"]
+
+db = TinyDB('randomuser.json')
+user = {
+    "first_name": first_name, 
+    "last_name": last_name, 
+    "age": age, "phone": phone, 
+    "country": country, 
+    "email": email}
+db.insert(user)
+
+User = Query()
+result = db.search(User.first_name == first_name)
+
+print(result)
