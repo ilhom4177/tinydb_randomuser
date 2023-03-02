e18d8847258807f9b2c29095852b3a39675aca8d
ilhom <ilhommamatqulov276@gmail.com>
2023-03-02 17:14:39 +0500

    m✊

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import requests
 from tinydb import TinyDB, Query
-param={
-    'results':5
-}
-response = requests.get("https://randomuser.me/api/",params=param)
-data = response.json()["results"][0]
-print(data)
 
-first_name = data["name"]["first"]
-last_name = data["name"]["last"]
-age = data["dob"]["age"]
-phone = data["phone"]
-country = data["location"]["country"]
-email = data["email"]
+def random_user(n):
+    db = TinyDB('randomuser.json', indent=4)
+    param = {'results': n}
+    response = requests.get("https://randomuser.me/api/", params=param)
+    data = response.json()["results"]
+    for user_data in data:
+        first_name = user_data["name"]["first"]
+        last_name = user_data["name"]["last"]
+        age = user_data["dob"]["age"]
+        phone = user_data["phone"]
+        country = user_data["location"]["country"]
+        email = user_data["email"]
 
-db = TinyDB('randomuser.json')
-user = {
-    "first_name": first_name, 
-    "last_name": last_name, 
-    "age": age, 
-    "phone": phone, 
-    "country": country, 
-    "email": email}
-db.insert(user)
+        user = {
+            "first_name": first_name, 
+            "last_name": last_name, 
+            "age": age, 
+            "phone": phone, 
+            "country": country, 
+            "email": email
+        }
+        db.insert(user)
+
+random_user(100)
